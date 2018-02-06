b8db55148a51f470b331402589f6b655e0a22077
wchoi32 <wchoi32@illinois.edu>
2018-02-05 17:02:40 -0800

    Pass all tests for stub spec, and handle get requests for live server integrations

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,5 +1,6 @@
 /* Import node's http module: */
 var http = require('http');
+var handleRequest = require('./request-handler.js');
 
 
 // Every server needs to listen on a port with a unique number. The
@@ -15,14 +16,13 @@ var port = 3000;
 var ip = '127.0.0.1';
 
 
-
 // We use node's http module to create a server.
 //
 // The function we pass to http.createServer will be used to handle all
 // incoming requests.
 //
 // After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handleRequest);
+var server = http.createServer(handleRequest.requestHandler);
 console.log('Listening on http://' + ip + ':' + port);
 server.listen(port, ip);
 
--- server/request-handler.js
+++ server/request-handler.js
@@ -11,6 +11,13 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+var messageArray = [];
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10 // Seconds.
+};
 
 var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
@@ -39,11 +46,33 @@ var requestHandler = function(request, response) {
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
+  headers['Content-Type'] = 'application/json';
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  
+  if (request.method === 'POST') {
+    console.log(request._postData);
+    if (request._postData) {
+      messageArray.push(request._postData);
+    }
+    response.writeHead(201);
+    response.end('post world');
+    return;
+  } else if (request.method === 'GET') { 
+    response.writeHead(statusCode, headers);
+    var jsonObj = JSON.stringify({
+      results: messageArray
+    });
+    response.end(jsonObj);
+  }
+  
+  
+  // response.writeHead(statusCode, headers);
+  // var testArr = ['result', 'another'];
+  // var jsonObj = JSON.stringify({
+  //   results: testArr
+  // });
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,7 +81,7 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  // response.end(jsonObj);
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -64,10 +93,7 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
+
+// exports.defaultCorsHeaders = defaultCorsHeaders;
+exports.requestHandler = requestHandler;
 
